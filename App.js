acdbfd7370473465ff9ad396868e68c3626bae7d
Ramon Lima <pedroramon.r.lima@gmail.com>
2018-09-13 18:02:05 -0300

    layout 1.0

--- App.js
+++ App.js
@@ -7,33 +7,42 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Button, View} from 'react-native';
+import {
+  StyleSheet,
+  StatusBar,
+  View, 
+  ImageBackground,
+  Image,
+  TouchableOpacity} from 'react-native';
 import Video from 'react-native-video';
 export default class App extends Component {
   constructor(props){
     super(props);
-    this.state = {play:true,label:'Play'}
+    this.state = {play:true}
     this.playRadio = this.playRadio.bind(this);
   }
 
   playRadio(){
     let radioState = this.state;
-    if(radioState.play){
-        radioState.play = false;
-        radioState.label = "stop";
-    }else {
-      radioState.play = true;
-      radioState.label = "play";
-    }
+    radioState.play = (this.state.play) ? false : true;
     this.setState(radioState);
   }
   render() {
     return (
-      <View style={styles.container}>
+      
+      <ImageBackground
+      source={require('./images/backgroundapp.jpg')}
+      style={styles.container}>
+      <StatusBar backgroundColor="#22315c" />
+      <View styles={styles.contimage}>
 
-        <Button title={this.state.label} onPress={this.playRadio} />
-
-          <Video source={{uri: "http://192.99.18.13:8858/live"}}   // Can be a URL or a local file.
+       <Image
+      source={require('./images/logo.png')}
+      style={styles.logo}>
+      
+      
+      </Image>
+      <Video source={{uri: "http://192.99.18.13:8858/live"}}   // Can be a URL or a local file.
               ref={(ref) => {
               this.player = ref
               }}                                      // Store reference
@@ -43,24 +52,59 @@ export default class App extends Component {
               onError={this.videoError}               // Callback when video cannot be loaded
               audioOnly={true}
               paused={this.state.play} />
-        
       </View>
+      
+      <TouchableOpacity onPress={this.playRadio}>
+      <Image
+        style={styles.botao}
+        source={(this.state.play) ? 
+          require("./images/playbotao.png")
+          :require("./images/stopbotao.png")}
+      />
+    </TouchableOpacity>
+    
+
+      </ImageBackground>
+    
+        
+
+
     );
+    
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    width:'100%',
+    height: '100%',
   },
-  backgroundVideo: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    bottom: 0,
-    right: 0,
+  logo: {
+    flex: 1,
+    resizeMode: 'contain',
+    width: " 100%",
+    height: '100%',
+    marginTop: '30%',
+    paddingTop:50,
+    padding: 60,
+    top: 60,
+    justifyContent: 'center',
+  },
+  botao:{
+  
+      alignItems: 'center',
+      width:'20%',
+      height:'20%',
+      marginTop: '50%',
+      marginLeft: 160,
+      
+     
+    
+    
+
+
+
+  
   }
-});
+})

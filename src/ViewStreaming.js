af20771282db965671b8232e338bee8cf7d54f76
Ramon Lima <pedroramon.r.lima@gmail.com>
2018-09-15 01:45:59 -0300

    primeiro erro corrigido

--- src/ViewStreaming.js
+++ src/ViewStreaming.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import {
   StyleSheet,
   StatusBar,
-  View, 
+  View,
+  Text, 
   ImageBackground,
   Image,
   TouchableOpacity} from 'react-native';
@@ -30,7 +31,7 @@ export default class ViewStreaming extends Component {
 
   static navigationOptions = ({navigation}) => ({
     headerTransparent: true,
-    headerLeft: BtnMenu()
+    //headerLeft: BtnMenu()
     
   });
 
@@ -42,42 +43,12 @@ export default class ViewStreaming extends Component {
   render() {
     return (
       
-      <ImageBackground
-      source={require('../images/backgroundapp.jpg')}
-      style={styles.container}>
-      <StatusBar backgroundColor="#22315c" />
-      <View styles={styles.contimage}>
-
-       <Image
-      source={require('../images/logo.png')}
-      style={styles.logo}>
-      
-      
-      </Image>
-      <Video source={{uri: "http://192.99.18.13:8858/live"}}   // Can be a URL or a local file.
-              ref={(ref) => {
-              this.player = ref
-              }}                                      // Store reference
-              playInBackground={true}
-              onBuffer={this.onBuffer}                // Callback when remote video is buffering
-              onEnd={this.onEnd}                      // Callback when playback finishes
-              onError={this.videoError}               // Callback when video cannot be loaded
-              audioOnly={true}
-              muted={this.state.play} />
+      <View style={styles.container}>
+          <View style={styles.contmenu}>
+              <Text>Texto Menu</Text>
+          
+          </View>
       </View>
-      <View style={styles.areaBtn}>
-          <TouchableOpacity onPress={this.playRadio}>
-          <Image
-            style={styles.botao}
-            source={(this.state.play) ? 
-              require("../images/playbotao.png")
-              :require("../images/stopbotao.png")}
-          />
-          </TouchableOpacity>
-    </View>
-    
-
-      </ImageBackground>
     
         
 
@@ -90,27 +61,8 @@ export default class ViewStreaming extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
   },
-  contimage:{
-    flex: 1,
-  },
-  areaBtn:{
-    flex:1,
-    justifyContent: 'flex-end',
-    
-    
-  },
-  logo: {
-    marginTop:50,
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  botao:{
-  
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    marginBottom:50
+  contmenu:{
+    backgroundColor:'green'
   }
 })

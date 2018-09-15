4d7a95c9a354ea9b8e3e1c7aaf2bc2160378504c
Ramon Lima <pedroramon.r.lima@gmail.com>
2018-09-15 09:42:07 -0300

    tabNavigation

--- src/MenuNavegacao.js
+++ src/MenuNavegacao.js
@@ -3,9 +3,9 @@ import { View, Text } from 'react-native';
 import { YellowBox } from 'react-native';
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 import ViewStreaming from './ViewStreaming'
-import { createStackNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation';
 
-const MenuNavegacao = createStackNavigator({
+const MenuNavegacao = createBottomTabNavigator({
     Home: {
         screen: ViewStreaming,
     },
